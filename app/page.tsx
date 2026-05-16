5ada0a7c0974995a754c6dfb97309f2f72ff6a06
Your Name <senzoradebe999@gmail.com>
2026-05-16 16:55:14 +0200

    Add authentication - login page and Supabase client

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 ﻿'use client'
 
 import { useState, useEffect } from 'react'
-
+import { createClient } from '../lib/supabase'
 export default function LandingPage() {
   const [time, setTime] = useState('')
 
